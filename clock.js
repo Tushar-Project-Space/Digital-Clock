83e95f798df2fd524c2488f175489d00bb49f506
Tushar-Project-Space <158036610+Tushar-Project-Space@users.noreply.github.com>
2024-02-02 17:02:23 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,14 @@
+function clock () {
+    const now = new Date();
+    let hours = now.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+    document.querySelector(".clock").textContent = timeString;
+}
+
+clock();
+setInterval(clock,1000);
